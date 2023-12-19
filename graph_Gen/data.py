ffc35be35df7bc7e35f07b0c1c8ca72c22ecb677
BehavioralComputing <154305180+BehavioralComputing@users.noreply.github.com>
2023-12-19 15:38:24 +0800

    Update data.py

--- graph_Gen/data.py
+++ graph_Gen/data.py
@@ -11,7 +11,6 @@ class GraphAdjSampler(torch.utils.data.Dataset):
 
         for G in G_list:
             adj = nx.to_numpy_matrix(G)
-            # the diagonal entries are 1 since they denote node probability
             self.adj_all.append(
                     np.asarray(adj) + np.identity(G.number_of_nodes()))
             self.len_all.append(G.number_of_nodes())
@@ -47,10 +46,6 @@ class GraphAdjSampler(torch.utils.data.Dataset):
         node_idx = 0
         
         adj_vectorized = adj_padded[np.triu(np.ones((self.max_num_nodes,self.max_num_nodes)) ) == 1]
-        # the following 2 lines recover the upper triangle of the adj matrix
-        #recovered = np.zeros((self.max_num_nodes, self.max_num_nodes))
-        #recovered[np.triu(np.ones((self.max_num_nodes, self.max_num_nodes)) ) == 1] = adj_vectorized
-        #print(recovered)
         
         return {'adj':adj_padded,
                 'adj_decoded':adj_vectorized, 
